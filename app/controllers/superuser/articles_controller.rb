534b53998c42489d12788a3ee6a7df190b7d9461
Trung <nguyenquoctrungcmu@gmail.com>
2018-07-18 22:53:38 +0700

    delete article

--- app/controllers/superuser/articles_controller.rb
+++ app/controllers/superuser/articles_controller.rb
@@ -11,11 +11,11 @@ module Superuser
     end
 
     def edit
-      @article = Article.find(params[:id])
+      @article = Article.find(id)
     end
 
     def update
-      @article = Article.find(params[:id])
+      @article = Article.find(id)
       if @article.update_attributes(article_params)
         redirect_to edit_superuser_article_path, notice: "Update successfully"
       else
@@ -32,7 +32,18 @@ module Superuser
     	end
     end
 
+    def destroy
+      @article = Article.find id
+      @article.destroy
+      flash[:notice] = 'Article has been deleted successfully.'
+      redirect_to superuser_articles_path
+    end
+
     private
+
+    def id
+      params.require(:id)
+    end
 
     def article_params
     	params.require(:article).permit(:content,:title, :category_id, :image)
